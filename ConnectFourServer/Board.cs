36ba16351fe316026b27d7ea9e5e01ab83c82505
youssefMHanna <youssef.m.hanna@outlook.com>
2022-02-06 01:43:53 +0200

    Server Initial Commit

---
+++ ConnectFourServer/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFourServer
+{
+    class Board
+    {
+        int[,] matrix;
+        //int rows, columns;
+
+        public Board(int rows, int columns)
+        {
+            matrix = new int[rows, columns];
+        }
+        private bool checkWinCondition(int x, int y)
+        {
+            return false;
+        }
+        public bool play(int x, int y , int TokenColor)
+        {
+            matrix[x,y] = TokenColor;
+            return !checkWinCondition(x, y);
+        }
+    }
+}
---
+++ ConnectFourServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFourServer
+{
+    class Program
+    {
+        List<Room> room = new List<Room>(); 
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
