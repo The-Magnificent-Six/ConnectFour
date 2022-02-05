50c031b320315a6df4b06e85c860df0c58da1d7e
Yousif Whby <YousifWhby@gmail.com>
2022-02-05 20:45:59 +0200

    1st finished version of Login class

---
+++ MainUIGame/Login.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainUIGame
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Clearfield_Click(object sender, EventArgs e)
+        {
+            UsrName.Clear();
+           
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string s;
+            if (UsrName.Text!="")
+            {
+                s = UsrName.Text;
+            MessageBox.Show("working");
+            }
+            else
+            {
+                s = "Guest";
+                MessageBox.Show("Please enter a correct password or usernam");     
+
+            }
+            //Lobby lob = new FormT();
+            //lob.lb = s;
+            //this.Hide();
+            //lob.Show();
+        }
+    }
+}
