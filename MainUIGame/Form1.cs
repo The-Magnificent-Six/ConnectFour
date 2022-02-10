70265b539b9d05ca6a581923ce884cc7e8f3c6e5
waadelessawy <82984314+waadelessawy@users.noreply.github.com>
2022-02-10 17:03:43 +0200

    2nd commit

--- MainUIGame/Form1.cs
+++ MainUIGame/Form1.cs
@@ -17,7 +17,12 @@ namespace MainUIGame
         int roomcount;
         Button[] newButton = new Button[100];
         ListBox[] newlistbox = new ListBox[100];
+        Panel[] newPanel = new Panel[100];
         int j = 0;
+        int ticks;
+        int n = 5;
+
+        Label newlabel;
 
         // ListBox newlistbox;
 
@@ -34,7 +39,9 @@ namespace MainUIGame
             x = 150;
             y = 150;
             //int z;
-            roomcount = 4;
+            roomcount =0;
+            //timer
+            //timer1.Start();
 
 
 
@@ -45,20 +52,20 @@ namespace MainUIGame
             if (roomcount < 10)
             {
                 
-                Panel newPanel = new Panel();
-                newPanel.Location = new System.Drawing.Point(x, y);
-                newPanel.BackColor = Color.Transparent;
-                newPanel.Name = "panel2";
-                newPanel.Size = new System.Drawing.Size(100, 150);
-                newPanel.TabIndex = 5;
-                newPanel.BorderStyle = BorderStyle.None;
-                this.Controls.Add(newPanel);
+                newPanel[j] = new Panel();
+                newPanel[j].Location = new System.Drawing.Point(x, y);
+                newPanel[j].BackColor = Color.Transparent;
+                newPanel[j].Name = "panel2";
+                newPanel[j].Size = new System.Drawing.Size(100, 150);
+                newPanel[j].TabIndex = 5;
+                newPanel[j].BorderStyle = BorderStyle.None;
+                this.Controls.Add(newPanel[j]);
                 x += 235;
 
                 if (x >= this.Width - 200)
                 { x -= (this.Width - 200); y += 230; }
 
-                EventArgs n;
+                
                 newButton[j] = new Button();
                 newButton[j].Text = "join";
                 newButton[j].BackColor = Color.DarkSlateGray;
@@ -66,9 +73,9 @@ namespace MainUIGame
                 newButton[j].Size = new System.Drawing.Size(60, 30);
                 newButton[j].Click += NewButton_Click2;
                 newButton[j].TabIndex = j;
-                newPanel.Controls.Add(newButton[j]);
+                newPanel[j].Controls.Add(newButton[j]);
 
-                Label newlabel = new Label();
+                newlabel = new Label();
                 newlabel.AutoSize = true;
                 newlabel.Location = new System.Drawing.Point(5, 5);
                 newlabel.Name = "newlabel";
@@ -76,7 +83,7 @@ namespace MainUIGame
                 newlabel.ForeColor = Color.White;
                 newlabel.TabIndex = 1;
                 newlabel.Text = "Room players";
-                newPanel.Controls.Add(newlabel);
+                newPanel[j].Controls.Add(newlabel);
 
                 
                 newlistbox[j] = new ListBox();
@@ -89,7 +96,7 @@ namespace MainUIGame
                 newlistbox[j].TabIndex = j;
 
 
-                newPanel.Controls.Add(newlistbox[j]);
+                newPanel[j].Controls.Add(newlistbox[j]);
 
                 roomcount++;
                 j++;
@@ -112,65 +119,102 @@ namespace MainUIGame
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //int j = 1;
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    Panel newPanel = new Panel();
-            //    newPanel.Location = new System.Drawing.Point(x, y);
-            //    newPanel.BackColor = Color.Transparent;
-            //    newPanel.Name = "panel2";
-            //    newPanel.Size = new System.Drawing.Size(100, 150);
-            //    newPanel.TabIndex = 5;
-            //    this.Controls.Add(newPanel);
-            //    x += 235;
-            //    if (x >= this.Width - 200)
-            //    { x -= (this.Width - 200); y += 230; }
+           
+            EXrooms();
 
-
-
-
-            //    Button newButton = new Button();
-            //    newButton.Text = "join";
-            //    newButton.BackColor = Color.DarkSlateGray;
-            //    newButton.Location = new System.Drawing.Point(20, 100);
-            //    newButton.Size = new System.Drawing.Size(60, 30);
-            //    newButton.Click += NewButton_Click1; ;
-            //    newPanel.Controls.Add(newButton);
-
-            //    Label newlabel = new Label();
-            //    newlabel.AutoSize = true;
-            //    newlabel.Location = new System.Drawing.Point(5, 5);
-            //    newlabel.Name = "newlabel";
-            //    newlabel.Size = new System.Drawing.Size(50, 50);
-            //    newlabel.TabIndex = 1;
-            //    newlabel.Text = "Room players";
-            //    newlabel.ForeColor = Color.White;
-
-            //    newPanel.Controls.Add(newlabel);
-
-            //    newlistbox = new ListBox();
-            //    newlistbox.FormattingEnabled = true;
-            //    newlistbox.ItemHeight = 20;
-            //    newlistbox.Location = new System.Drawing.Point(0, 20);
-            //    newlistbox.Name = "lnewlistbox";
-            //    newlistbox.Size = new System.Drawing.Size(120, 100);
-            //    newlistbox.TabIndex = 4;
-            //    newlistbox.BackColor = Color.DarkBlue;
-
-            //    newPanel.Controls.Add(newlistbox);
-            //}
         }
 
-        private void NewButton_Click1(object sender, EventArgs e)
-        {
-            //this.newlistbox.Items.Add("player1");
-        }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
             Invalidate();
         }
 
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            ticks++;
 
+            if (ticks == 5)
+            { 
+                timer1.Stop();
+                for (int i = 0; i < 5; i++)
+                {
+                    newPanel[i].Dispose();
+
+                }
+
+                //EXrooms();
+                Form1_Load(null, EventArgs.Empty);
+            }
+            
+        }
+        private void EXrooms()
+        {
+            
+
+            ticks = 0;
+            timer1.Start();
+            
+            
+            for (int i = 0; i < n; i++)
+            {
+                
+                newPanel[j] = new Panel();
+                newPanel[j].Location = new System.Drawing.Point(x, y);
+                newPanel[j].BackColor = Color.Transparent;
+                newPanel[j].Name = "panel2";
+                newPanel[j].Size = new System.Drawing.Size(100, 150);
+                newPanel[j].TabIndex = 5;
+                this.Controls.Add(newPanel[j]);
+                x += 235;
+                if (x >= this.Width - 200)
+                { x -= (this.Width - 200); y += 230; }
+
+
+
+
+                newButton[j] = new Button();
+                newButton[j].Text = "join";
+                newButton[j].BackColor = Color.DarkSlateGray;
+                newButton[j].Location = new System.Drawing.Point(20, 100);
+                newButton[j].Size = new System.Drawing.Size(60, 30);
+                newButton[j].Click += NewButton_Click2;
+                newButton[j].TabIndex = j;
+                newPanel[j].Controls.Add(newButton[j]);
+
+                newlabel = new Label();
+                newlabel.AutoSize = true;
+                newlabel.Location = new System.Drawing.Point(5, 5);
+                newlabel.Name = "newlabel";
+                newlabel.Size = new System.Drawing.Size(50, 50);
+                newlabel.TabIndex = 1;
+                newlabel.Text = "Room players";
+                newlabel.ForeColor = Color.White;
+
+                newPanel[j].Controls.Add(newlabel);
+
+                newlistbox[j] = new ListBox();
+                newlistbox[j].FormattingEnabled = true;
+                newlistbox[j].ItemHeight = 20;
+                newlistbox[j].Location = new System.Drawing.Point(0, 20);
+                newlistbox[j].Name = "lnewlistbox";
+                newlistbox[j].Size = new System.Drawing.Size(120, 100);
+                newlistbox[j].BackColor = Color.DarkBlue;
+                newlistbox[j].TabIndex = j;
+
+
+                newPanel[j].Controls.Add(newlistbox[j]);
+
+                roomcount++;
+                j++;
+
+
+            }
+            x = 150;
+            y = 150;
+            if (n > 1)
+            { n--; }
+
+        }
     }
 }
