22ea2134a141fa3bd3849cc9196b21b79c951992
Mary Akram <marieesaa73@gmail.com>
2022-02-16 01:42:04 +0200

    Add project files.

---
+++ WindowsFormsApp4/GameBoard.Designer.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+namespace Client
+{
+    partial class GameBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // GameBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 635);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "GameBoard";
+            this.Text = "Form1";
+        //    this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GameBoard_FormClosing);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ WindowsFormsApp4/User.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    /// <summary>
+    ///  Responsible for all Connect4 Game client logic 
+    /// </summary>
+
+    public static class GameManger
+    {
+
+        static string ip;
+        static int port;
+        public static bool connStatues;
+        static string UserName;
+
+
+        static IPAddress ServerIP;
+        static TcpClient server;
+        static NetworkStream ConnectionStream;
+        static BinaryReader br;
+        static BinaryWriter bwr;
+        static Task recieve;
+
+        public static List<Player> playerslist;
+        public static List<Room> Rommslist;
+
+        public static Player CurrentPlayer;
+        public static Room CurrentRoom;
+
+
+
+        static GameManger()
+
+        {
+            ip = "127.0.0.1";
+            port = 2222;
+            connStatues = false;
+            ServerIP = IPAddress.Parse(ip);
+
+            playerslist = new List<Player>();
+            Rommslist = new List<Room>();
+
+        }
+
+        /// <summary>
+        ///  Connect the user to the server and return and return The result of the attempt 
+        /// </summary>
+
+        public static void Login(string userName)
+        {
+
+
+            try
+            {
+                TcpClient server = new TcpClient();
+                server.Connect(ServerIP, port);
+                ConnectionStream = server.GetStream();
+                br = new BinaryReader(ConnectionStream);
+                bwr = new BinaryWriter(ConnectionStream);
+
+             //   SendServerRequest(Flag.sendLoginInfo, userName);
+                UserName = userName;
+
+                //recieve = new Task(ReceiveServerRequest);
+                //recieve.Start();
+                //SendServerRequest(Flag.getPlayers);
+                //SendServerRequest(Flag.getRooms);
+
+
+
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+
+
+
+
+        }
+        /// <summary>
+        ///  This Method used for communicating with the game server
+        /// </summary>
+        public static void SendServerRequest(Flag flag, params string[] data)
+        {
+            var f = (int)flag;
+            string msg = f.ToString();
+
+
+            if (data.Length > 0)
+            {
+                foreach (var item in data)
+                {
+                    msg += "," + item;
+
+                }
+
+
+            }
+
+            bwr.Write(msg);
+            //MessageBox.Show(msg);
+
+        }
+
+        public static bool isloginSuc(string userName)
+        {
+
+            var msg = br.ReadString();
+            var msgArray = msg.Split(',');
+            Flag flag = (Flag)int.Parse(msgArray[0]);
+            var data = msgArray.ToList();
+            // MessageBox.Show(data.ElementAt(0) + "," + data.ElementAt(1));
+            data.RemoveAt(0);
+            if (data.ElementAt(0) == "1")
+            {
+                UserName = userName;
+              //  recieve = new Task(ReceiveServerRequest);
+                recieve.Start();
+               // SendServerRequest(Flag.getPlayers);
+              //  SendServerRequest(Flag.getRooms);
+
+                connStatues = true;
+
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("the name is already taking please use another one");
+                return false;
+            }
+        }
+
+
+
+
+        private static void playgame(string size)
+        {
+            var sizear = size.Split('+');
+           // lobby.mainlobby.Invoke(new MethodInvoker(delegate ()
+            {
+
+              //  lobby.mainlobby.Hide();
+
+                GameBoard.columns = int.Parse(sizear[0]);
+                GameBoard.rows = int.Parse(sizear[1]);
+                GameBoard.HostColor = Color.Red;
+                GameBoard.ChallangerColor = Color.Purple;
+                GameBoard.turn = 2;
+
+                // lobby.seegamebaord = new GameBoard();
+                // lobby.seegamebaord.Show();
+            };
+
+        }
+
+
+      
+      
+
+        /// <summary>
+        ///  Get all the players on the server and return them as an List of Players
+        /// </summary>
+
+        public static List<Player> Getplayers(List<string> data)
+        {
+            var players = new List<Player>();
+
+            foreach (var item in data)
+            {
+                var name = item.Split('+')[0];
+                players.Add(new Player(name));
+            }
+            return players;
+        }
+
+        /// <summary>
+        ///  Get all the players on the server and return them as an List of Players
+        /// </summary>
+
+        public static List<Room> GetRooms(List<string> data)
+        {
+            var rooms = new List<Room>();
+
+            foreach (var item in data)
+            {
+                var rom = item.Split('+');
+                var roomName = rom[0];
+                var host = rom[1].Split('-');
+                rooms.Add(new Room(roomName, new Player(host[0])));
+            }
+            return rooms;
+        }
+
+        /// <summary>
+        ///  Update the current Player INFO
+        /// </summary>
+
+        public static void UpdatePlayer(Color colorName)
+        {
+            var currPlayer = new Player(UserName);
+
+            currPlayer.PlayerColor = colorName;
+
+            CurrentPlayer = currPlayer;
+        }
+
+        /// <summary>
+        ///  Update the current Room INFO
+        /// </summary>
+
+        public static Room UpdateRoom(List<string> data)
+        {
+
+            var currroom = new Room(data[0], new Player(data[1]));
+
+
+            return currroom;
+        }
+
+
+    }
+
+    public class Player
+    {
+        public string Name { get; }
+        public Color PlayerColor { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+
+        }
+    }
+
+
+    public class Room
+    {
+        public string Name { get; set; }
+        public Player Host { get; set; }
+
+        public Player challenger { get; set; }
+        public Player[] inspectors { get; set; }
+
+        public Room(string name, Player host)
+        {
+            Name = name;
+            Host = host;
+        }
+
+    }
+    public enum Flag
+    {
+        availRoomsReq = 1,
+        createRoomReq = 2,
+        joinRoomAsPlayer = 3,
+        joinRoomAsSpectator = 4,
+        getWholeBoard = 5,
+        playerMoveReq = 6,
+        roomsResp = 7,
+        ErrorOrAccept = 8
+    }
+}
